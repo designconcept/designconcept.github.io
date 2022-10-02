22df3c1be66fa392e7fd00862c961321fa974753
designconcept <114839251+designconcept@users.noreply.github.com>
2022-10-02 02:19:56 +0100

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,117 @@
+/*const currentYear = 2023;
+const ageBasirah = currentYear - 1990;
+const ageHira = currentYear - 2008;
+console.log(ageBasirah, ageHira);
+
+console.log(ageBasirah*2 , ageBasirah/2, ageHira**3);
+
+const firstName = "Basirah";
+const lastName = "Ayoola";
+console.log(firstName + "  " + lastName);
+console.log(typeof firstName);
+console.log(typeof currentYear);
+let age = 25; // age = 25
+age += 10; // age = age + 10 =35
+age *= 2; //age=age(35)* 2 = 70
+age++; //age = age (70) + 1 = 71
+age--;
+age--;
+age--;
+console.log(age);
+
+console.log(ageBasirah < ageHira);
+console.log(ageBasirah > ageHira);
+console.log(ageBasirah >= ageHira);
+console.log(ageBasirah <= ageHira);
+
+const irfanMass = 78;
+const hannanMass = 92;
+const irfanHeight = 1.69;
+const hannanHeight = 1.95;
+const BMIrfan = irfanMass/ irfanHeight ** 2;
+const BMHannan = hannanMass/ hannanHeight ** 2;
+console.log (BMIrfan, BMHannan);
+const irfanVsHannan = BMIrfan > BMHannan;
+console.log(BMIrfan, BMHannan,irfanVsHannan);
+console.log(BMIrfan);
+console.log(BMHannan);
+
+const firstName = "John";
+const job = "Teacher"
+const birthYear = 1990;
+const year = 2022;
+
+//const John = " My name is " + firstName + " I am a " + job + " my age is " + (year - birthYear) + " years old.";
+//console.log(John);
+//console.log (`My name is ${firstName} I am a ${job} my age is ${year - birthYear} years old`);
+console.log('Here is line 1....  \n\
+Here is line 2.... \n\
+Here is line 3....');
+
+console.log(`Here is line 1..... 
+Here is line 2..... 
+Here is line 3.....`);*/
+
+
+/*const age = 15;
+if (age >= 18) {
+    console.log('Imran can drive');
+} else {
+    const yearsLeft = 18 - age;
+    console.log('Imran is too young to drive, he needs to wait for another ${yearsLeft} years');
+}
+
+
+const day = 'sunday';
+
+switch (day){
+    case 'monday':
+        console.log('Go to work');
+        break;
+case 'tuesday':
+ console.log('Go shopping');
+ break;
+ case 'wednesday':
+ console.log('Go and do some exercise');
+ break;
+ case 'thursday':
+ case 'friday':
+ console.log('Go to the park');
+ break;
+ case 'saturday':
+ case 'sunday':
+console.log('Spend time with family');
+break;
+}
+
+
+if (day ==='monday') {
+    console.log('Go to work');
+}
+else if (day ==='tuesday'){
+    console.log('Go shopping');
+}
+else if (day === 'wednesday'){
+    console.log('Go and do some exercise');
+}
+else if(day === 'thursday' || day === 'friday'){
+    console.log('Go to the park');
+}
+else if (day === 'saturday' || day === 'sunday'){
+    console.log('spend time with family');
+}*/
+
+/*console.log(`Here is my Website...`);
+const firstName = "Basirah";
+const job = "Designer"
+const birthYear = 1980;
+const year = 2022;
+
+const Basirah = " My name is " + firstName + " I am a " + job + " my age is " + (year - birthYear) + " years old.";
+console.log(Basirah);
+console.log (`My name is ${firstName} I am a ${job} my age is ${year - birthYear} years old`);*/
+/*function myFunction() {
+    document.getElementById("demo").style.fontSize = "30px"; 
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "bisque";      
+  }*/
